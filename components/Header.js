2d0fe9b964c2af1839e087d34624de1b662a12a7
Nadim Ahmed <enn001@DESKTOP-J7ABJRA.localdomain>
2022-11-14 20:13:32 +0000

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import { ConnectButton } from "web3uikit"
+
+export default function Header() {
+    return (
+        <div className="border-b-2 p-5 flex flex-row">
+            <h1 className="py-4 px-4 font-blog text-3xl">Decentralized Lottery</h1>
+            <div className="py-2 px-4 ml-auto">
+                <ConnectButton moralisAuth={false} />
+            </div>
+        </div>
+    )
+}
